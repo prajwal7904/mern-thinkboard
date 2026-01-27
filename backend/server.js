d1b0b6238791a1ed6f8622f0edc179b0a535eeef
preethesh <preethesh4263@gmail.com>
2026-01-27 20:39:47 +0530

    Fix Express wildcard route

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ if(process.env.NODE_ENV==="production"){
 app.use(express.static(path.join(__dirname,"../frontend/dlist")))
 
 
-app.get("*",(req,res)=>{
+app.get("/*",(req,res)=>{
     res.sendFile(path.join(__dirname,"../frontend","dlist","index.js"));
 })
 }
