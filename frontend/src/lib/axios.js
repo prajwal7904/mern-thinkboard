4f987594c96841e9e85c0439a6ad0fcba73d69ae
preethesh <preethesh4263@gmail.com>
2026-04-30 19:20:06 +0530

    update backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE ==="development"?"http://localhost:5000/api":"/api "
+const BASE_URL = import.meta.env.MODE ==="development"?"http://localhost:5000/api":"https://mern-thinkboard-12-l8u8.onrender.com/api "
 const api = axios.create({
     baseURL:BASE_URL,
 })
