4e0458294122de4479f358a09b7c74f01ca70ff0
preethesh <preethesh4263@gmail.com>
2026-04-30 19:37:46 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      
-    ],
+    origin: "https://mern-thinkboard-13-a076.onrender.com",
     credentials: true,
   })
 )
