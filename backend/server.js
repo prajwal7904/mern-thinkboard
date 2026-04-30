69468bafb2ccd780ce4237cc073a90833114f3dd
preethesh <preethesh4263@gmail.com>
2026-04-30 19:09:34 +0530

    update backend

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://69f30b0d573b96669e8ab17a--brilliant-vacherin-519e6d.netlify.app"
+      
     ],
     credentials: true,
   })
