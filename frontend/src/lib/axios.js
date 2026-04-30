71e033d39f2f353aa44823a3592c4ad63b1fc40a
preethesh <preethesh4263@gmail.com>
2026-04-30 19:33:21 +0530

    fix api url space

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE ==="development"?"http://localhost:5000/api":"https://mern-thinkboard-12-l8u8.onrender.com/api "
+const BASE_URL = import.meta.env.MODE ==="development"?"http://localhost:5000/api":"https://mern-thinkboard-12-l8u8.onrender.com/api"
 const api = axios.create({
     baseURL:BASE_URL,
 })
-export default api
+export default api<p></p>
