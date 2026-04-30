957d295a90592ad31d6339108d355055b57f5f9b
preethesh <preethesh4263@gmail.com>
2026-04-30 13:16:37 +0530

    fixed production api

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE ==="development"?"http://localhost:5000/api":"https://mern-thinkboard-7-t0dn.onrender.com"
+const BASE_URL = import.meta.env.MODE ==="development"?"http://localhost:5000/api":"https://mern-thinkboard-10-10af.onrender.com/api"
 const api = axios.create({
     baseURL:BASE_URL,
 })
