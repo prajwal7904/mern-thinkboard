f432bfa85f45dbb4bbec21877786420818dc8b13
preethesh <preethesh4263@gmail.com>
2026-04-30 13:21:54 +0530

    fixed cors syntax

--- backend/server.js
+++ backend/server.js
@@ -2,38 +2,31 @@ import express from 'express'
 import noteRoutes from './routes/noteRoutes.js'
 import dotenv from 'dotenv'
 import cors from 'cors';
-import {connectDb} from './config/db.js'
+import { connectDb } from './config/db.js'
 import rateLimiter from './middleware/rateLimiter.js'
-
 
 dotenv.config();
 
-const app =express()
-const PORT = process.env.PORT ||5000
+const app = express()
+const PORT = process.env.PORT || 5000
 
-
-
-if(process.env.NODE_ENV !=='production'){
 app.use(
-    cors({
-    origin:"http://localhost:5173",
-})) 
-}
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://69f308204dc1be754ee6f706--leafy-biscuit-425b8f.netlify.app"
+    ],
+    credentials: true,
+  })
+)
+
 app.use(express.json())
 app.use(rateLimiter)
 
+app.use("/api/notes", noteRoutes)
 
-app.use("/api/notes",noteRoutes)
-
-
-// Root route (health check)
-
-
-
-connectDb().then(()=>{
-    
-    app.listen(PORT,()=>{
-    console.log('Server running on port :',PORT);
-    
-})
-}); 
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server running on port :', PORT);
+  })
+});
